df2ffc40d4a9e21a045c67dd673322ea8fc07546
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-04 01:19:33 -0400

    Desafio2 program runs with successful tests

--- desafio_2.rb
+++ desafio_2.rb
@@ -12,3 +12,14 @@ Utilizando .map, crear un arreglo único con la cantidad de letras que tiene cad
 
 =end
 
+nombres = ['Violeta', 'Andino', 'Clemente','Javiera', 'Paula', 'Pía', 'Ray']
+
+nombres.select {|x| x.length > 5} #Obtener todos los elementos que excedan los 5 caracteres, utilizando .select
+
+nombres.map {|x| x.downcase} #Utilizar .map para crear un arreglo con todos los nombres en minúscula.
+
+nombres.select {|x| x if x[0] == "P"} #Utilizar .select para crear un arreglo con todos los nombres que empiecen con P
+
+nombres.count {|x| x[0] == "A" || x[0] == "B" || x[0] == "C"} #Utilizando .count, contar los elementos que empiecen con 'A', 'B' o 'C'.
+
+nombres.map {|x| x.length} #Utilizando .map, crear un arreglo único con la cantidad de letras que tiene cada nombre.
