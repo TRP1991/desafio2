ed1b3268384f2308bb19d3643510831901086a28
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-04 00:59:29 -0400

    start solution to the Desafio2 program

---
+++ desafio_2.rb
@@ -0,0 +1,14 @@
+=begin 
+
+Solución Desafio 2
+
+nombres = ['Violeta', 'Andino', 'Clemente','Javiera', 'Paula', 'Pía', 'Ray']
+
+Obtener todos los elementos que excedan los 5 caracteres, utilizando .select
+Utilizar .map para crear un arreglo con todos los nombres en minúscula.
+Utilizar .select para crear un arreglo con todos los nombres que empiecen con P
+Utilizando .count, contar los elementos que empiecen con 'A', 'B' o 'C'.
+Utilizando .map, crear un arreglo único con la cantidad de letras que tiene cada nombre.
+
+=end
+
